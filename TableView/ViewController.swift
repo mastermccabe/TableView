e9b3af31823ad10fd77813ffdf300ae6467bf5a3
McCabe <mccabe@wambl.com>
2017-11-06 14:35:53 -0800

    working

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -9,9 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var tasks = ["something", "something else", "third"]
+    
+     var inputText = ""
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var taskTextfield: UITextField!
+    
+
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+
+        
+        inputText = taskTextfield.text!
+        tasks.append(inputText)
+        print("in append",inputText)
+        tableView.reloadData()
+        taskTextfield.text = ""
+        
+//        public mutating func append(_ taskTextfield.text: String) {
+//            tasks)
+//        }
+//        tableView.reloadData()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -23,3 +49,19 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+   
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+
+    }
+   
+}
